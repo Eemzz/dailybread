b16fa509416f12bbe1471c51fd5245f705635517
Eemzz <e.jaludi@gmail.com>
2022-10-29 22:09:09 -0700

    fixed accountScreen display

--- app/src/main/java/com/example/dailybread/user/UserManager.kt
+++ app/src/main/java/com/example/dailybread/user/UserManager.kt
@@ -1,9 +1,16 @@
 package com.example.dailybread.user
 
+import androidx.compose.runtime.Composable
+
 object UserManager {
     //TODO remove for session management
     var isUserLoggedIn = true
     fun isLoggedIn(): Boolean {
         return isUserLoggedIn
     }
+
+}
+@Composable
+fun ChangePassword(oldPassword: String, newPassword: String ){
+    //TODO
 }
